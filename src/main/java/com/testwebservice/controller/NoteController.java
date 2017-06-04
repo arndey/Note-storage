003013fe45b566980265e805ca215e1b1f91a864
Andrey <andreykostyuchenko74@gmail.com>
2017-06-04 18:22:33 +0600

    minor changes

--- src/main/java/com/testwebservice/controller/NoteController.java
+++ src/main/java/com/testwebservice/controller/NoteController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class WelcomeController {
+public class NoteController {
     @Autowired
     private NoteDao noteDao;
 
